623b8c17083eaa3699ec1b45dd441f5c5ec165c5
gabrielsbu <gabrielmaiasbu@gmail.com>
2020-10-20 22:13:47 -0300

    Responsividade page user quase pronta

--- src/pages/UserMap/index.tsx
+++ src/pages/UserMap/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { FiArrowLeft, FiDelete } from 'react-icons/fi';
+import { useParams } from 'react-router-dom';
+import { FiArrowLeft, FiDelete, FiLink } from 'react-icons/fi';
+
+import { useHistory } from 'react-router-dom';
 
 import { Map, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
@@ -12,7 +14,7 @@ interface User{
     id: number;
     login: string;
     avatar_url: string;
-    url: string;
+    html_url: string;
     name: string;
     bio:string;
     repos: Array<{
@@ -38,6 +40,8 @@ interface UserParams{
 }
 
 function UserMap(){
+
+    const { goBack } = useHistory();
     const params = useParams<UserParams>();
     const [user, setUser] = useState<User>();
     const [list, setList] = useState<Repos[]>([]);
@@ -70,51 +74,80 @@ function UserMap(){
     if(!list) {
         return <p>Carregando ...</p>
     }
+
+
     return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <div className="config-header">
-                        <img src={user.avatar_url} alt={user.name}/>
-                        <span>@{user.login}</span>
+        <div className="page-map">
+            <header>
+                <div className="profile-desktop">
+                    
+                    <img src={user.avatar_url} alt={user.name}/>
+
+                    <div>
+                        <h3>{user.name}</h3>
+                        <p>@{user.login}</p>
+                        <h4>{user.bio}</h4>
+                        <span> <a target="_blank" rel="noopener noreferrer" href={user.html_url}> <FiLink />{user.html_url}</a></span>
                     </div>
+                </div>
 
-                    <div className="config-header-infos">
-                        <span>{user.bio}</span>
-                        <span>{user.url}</span>
-                    </div>
-                </header>
+                <div className="profile-mobile">
+                    <button className="config-button" onClick={goBack}>
+                        <FiArrowLeft size={32} color="#fff"/>
+                    </button>
+                    
+                    <img src={user.avatar_url} alt={user.name}/>
+                    <h3>{user.name}</h3>
+                    <p>@{user.login}</p>
+                    <h4>{user.bio}</h4>
+                    <span> <a target="_blank" rel="noopener noreferrer" href={user.html_url}> <FiLink />{user.html_url}</a></span>
+                    
+                </div>
+                
+                <button className="config-button" onClick={goBack}>
+                    <FiArrowLeft size={32} color="#fff"/>
+                </button>
+            </header>
 
 
-                <main>
-                    <h4>Stars</h4>
+            <main>
+                <div className="map">
+                    
+                    <h3>Localização</h3>
+
+                    <Map 
+                        center={[-4.8415535,-37.7896551]}
+                        zoom={14}
+                    >
+                    
+                    <TileLayer 
+                        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                    />
+                    </Map>
+                </div>
+
+                <div className="unfavorite">
+
+                    <h3>Stars</h3>
+                    
                     {list.map(rep => {
                         return (
-                            <div key={rep.id} className="config-dislike">
-                                <p>{rep.name} </p>
-                                <button onClick={() => handleDislike(rep.id)}><FiDelete /><span>Remove</span></button>
+                            <div key={rep.id} className="config-unfavorite">
+                                <span>
+                                    <p>{rep.name} </p>
+                                    <FiDelete onClick={() => handleDislike(rep.id)} /> 
+                                </span>
+                                
                             </div>
                         );
                     })}
-                    
-                </main>
-            </aside>
+                </div>
 
-            <Map 
-                center={[-4.8415535,-37.7896551]}
-                zoom={14}
-                style={{ width: '100%', height: '100%'}}
-            >
-                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
-                <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
-            </Map>
+                
+
+                
+            </main>
             
-            <Link to="" className="create-orphanage">
-                <FiArrowLeft size={32} color="#fff"/>
-            </Link>
-           
         </div>
     );
 }
